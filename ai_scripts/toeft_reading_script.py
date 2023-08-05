095fb64f200b36c615f1006be60c55ba842bf5b3
belek <belek.ayazbekov@cloudera.com>
2023-08-06 03:30:25 +0600

    Toefl reading test json response

--- ai_scripts/toeft_reading_script.py
+++ ai_scripts/toeft_reading_script.py
@@ -1,32 +1,32 @@
 import openai
-
+import re
+import json
 
 def getToeflTest():
     openai.api_key = "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"
 
     passage_response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-16k",
         messages=[
             {
                 "role": "system",
-                "content": "You are a skilled AI capable of generating a detailed, academic passage between 500-700 words within the realms of humanities, social sciences, life sciences, or physical sciences. Provide the title and the passage itself"
+                "content": "You are a skilled AI capable of generating a title and a detailed, academic article between 500-700 words within the realms of humanities, social sciences, life sciences, or physical sciences.The passage should be continuous and it should not specify the names of the subsections"
             },
             {
                 "role": "user",
-                "content": "Please generate an academic passage for me."
+                "content": "Generate a title and corresponding academic passage."
             },
         ],
         temperature=1,
-        max_tokens=3500
+        max_tokens=5000
     )
 
-    passage = passage_response['choices'][0]['message']['content']
+    title_and_passage = passage_response['choices'][0]['message']['content']
 
-    print("Generated Passage: ", passage)
 
     # Generate the questions and answers
     qa_response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-16k",
         messages=[
             {
                 "role": "system",
@@ -34,14 +34,44 @@ def getToeflTest():
             },
             {
                 "role": "user",
-                "content": f"Based on this passage, generate 10 reading comprehension questions with 4 answer choices each, and also provide an 'Answer Key'.\n\n{passage}"
+                "content": f"Based on the following passage, generate 10 reading comprehension questions each with 4 answer choices, and provide an 'Answer Key'.\n\n{title_and_passage}"
             },
         ],
-        temperature=0.7,
-        max_tokens=3000
+        temperature=1,
+        max_tokens=5000
     )
 
-    print("Questions, Answers, and Answer Key: ", qa_response['choices'][0]['message']['content'])
+    question_answer = qa_response['choices'][0]['message']['content']
+
+    json_response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a skilled AI capable of taking a given title, passage, questions, and answer key and generating a structured output similar to a JSON."
+            },
+            {
+                "role": "user",
+                "content": f"Take the following title, passage, questions, and answer key and format them into a structured output similar to a JSON. Do not shorten anything. The Json should have these parameters: title, passage, questions: question, options, answer(which contains only the letter). Do not shorten anything f:\n\n{title_and_passage}\n\n{question_answer}"
+            },
+        ],
+        temperature=1,
+        max_tokens=12000
+    )
+
+    json_like_output = json_response['choices'][0]['message']['content']
+    try:
+        # Remove unnecessary characters such as new lines or spaces at the beginning and end
+        json_like_output = json_like_output.strip()
+
+        # Convert the string to JSON
+        json_object = json.loads(json_like_output)
+    except json.JSONDecodeError:
+        print("Decoding JSON has failed")
+
+    return json.dumps(json_object)
 
 
-getToeflTest()
+
+
+
--- learningPlatformApp/views.py
+++ learningPlatformApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from ai_scripts import toeft_reading_script
 
 
-def hello_world(request):
-    return HttpResponse("Hello, world!")
+def getToeflTest(request):
+    response = toeft_reading_script.getToeflTest()
+    return HttpResponse(response, content_type="application/json")

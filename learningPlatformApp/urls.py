d4c6a02b336c1955483467656445cb4a5708f238
belek <belek.ayazbekov@cloudera.com>
2023-08-06 13:29:24 +0600

    Analogy

--- learningPlatformApp/urls.py
+++ learningPlatformApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('tests/toefl/reading', views.getToeflTest),
+    path('tests/ort/analogy', views.get_ort_analogy_test)
 ]
--- learningPlatformApp/views.py
+++ learningPlatformApp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from ai_scripts import toeft_reading_script
+from ai_scripts import toeft_reading_script, ort_analogy
 
 
 def getToeflTest(request):
     response = toeft_reading_script.getToeflTest()
+    return HttpResponse(response, content_type="application/json")
+
+
+def get_ort_analogy_test(request):
+    response = ort_analogy.get_ort_analogy_test()
     return HttpResponse(response, content_type="application/json")
